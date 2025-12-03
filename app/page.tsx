ca78cd5f0b85477deb42a112ed7464a33a875113
25LP5143 25LP5143 <25LP5143@G305-CC0240.local>
2025-12-03 12:34:45 +0800

    sdasd

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import { FileText, RotateCw, Sparkles } from "lucide-react";
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { FileText, RotateCw, Sparkles, Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -12,163 +16,221 @@ import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState("");
+
+  const handleImageChange = (event) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      setSelectedImage(file);
+
+      setPreviewUrl(URL.createObjectURL(file));
+    }
+  };
+  const removeSelectedImage = () => {
+    setSelectedImage(null);
+
+    if (previewUrl) {
+      URL.revokeObjectURL(previewUrl);
+    }
+    setPreviewUrl("");
+  };
   return (
-    <div className="flex w-full justify-center items-center">
-      <div className="flex w-full max-w-[580px] flex-col gap-6">
-        <Tabs defaultValue="account">
-          <TabsList>
-            <TabsTrigger value="Image analysis">Image analysis</TabsTrigger>
-            <TabsTrigger value="Ingredient recognition">
-              Ingredient recognition
-            </TabsTrigger>
-            <TabsTrigger value="Image creator">Image creator</TabsTrigger>
-          </TabsList>
-          <TabsContent value="Image analysis">
-            <Card>
-              <CardHeader>
-                <CardTitle>
-                  <div className="flex w-full h-fit justify-between">
-                    <div className="flex gap-2 justify-center items-center text-lg font-bold">
-                      <Sparkles />
-                      Image analysis
+    <div>
+      <div className="flex w-full justify-start items-center">
+        <h1 className="p-4 pl-12 text-xl font-bold">AI tools</h1>
+      </div>
+      <hr className="w-full" />
+      <div className="flex w-full justify-center items-center">
+        <div className="flex w-full max-w-[580px] flex-col gap-6">
+          <Tabs defaultValue="account">
+            <TabsList>
+              <TabsTrigger value="Image analysis">Image analysis</TabsTrigger>
+              <TabsTrigger value="Ingredient recognition">
+                Ingredient recognition
+              </TabsTrigger>
+              <TabsTrigger value="Image creator">Image creator</TabsTrigger>
+            </TabsList>
+            <TabsContent value="Image analysis">
+              <Card className="border-none shadow-none ">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex w-full h-fit justify-between">
+                      <div className="flex gap-2 justify-center items-center text-lg font-bold">
+                        <Sparkles />
+                        Image analysis
+                      </div>
+                      <Button
+                        className="w-12 h-10"
+                        variant="outline"
+                        size="icon"
+                        aria-label="Submit"
+                      >
+                        <RotateCw />
+                      </Button>
                     </div>
-                    <Button
-                      className="w-12 h-10"
-                      variant="outline"
-                      size="icon"
-                      aria-label="Submit"
-                    >
-                      <RotateCw />
-                    </Button>
-                  </div>
-                </CardTitle>
-                <CardDescription>
-                  Upload a food photo, and AI will detect the ingredients.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="grid gap-6">
-                <div className="grid gap-3">
-                  <Input
-                    className="h-10"
-                    id="tabs-demo-name"
-                    placeholder="JPG , PNG"
-                    type="file"
-                  />
-                </div>
-                <div className="flex justify-end">
-                  <Button className="h-10">Generate</Button>
-                </div>
-              </CardContent>
-              <div>
-                <CardFooter>
+                  </CardTitle>
                   <CardDescription>
-                    <div className="flex text-lg font-bold text-black gap-2">
-                      <FileText />
-                      Here is the summary
-                    </div>
                     Upload a food photo, and AI will detect the ingredients.
                   </CardDescription>
-                </CardFooter>
-              </div>
-            </Card>
-          </TabsContent>
-          <TabsContent value="Ingredient recognition">
-            <Card>
-              <CardHeader>
-                <CardTitle>
-                  <div className="flex w-full h-fit justify-between">
-                    <div className="flex gap-2 justify-center items-center text-lg font-bold">
-                      <Sparkles />
-                      Ingredient recognition
-                    </div>
-                    <Button
-                      className="w-12 h-10"
-                      variant="outline"
-                      size="icon"
-                      aria-label="Submit"
-                    >
-                      <RotateCw />
+                </CardHeader>
+                <CardContent className="grid gap-6">
+                  <div className="grid gap-3">
+                    <Input
+                      className="h-10"
+                      id="tabs-demo-name"
+                      placeholder="JPG , PNG"
+                      accept="image/*" // Restrict file selection to images
+                      type="file"
+                      onChange={handleImageChange}
+                    />
+                    {previewUrl && (
+                      <div style={{ marginTop: "5px" }}>
+                        <h3 className="text-[#71717A] text-sm p-2">
+                          Upload a food photo, and AI will detect the
+                          ingredients.
+                        </h3>
+                        <div className="flex relative w-full justify-end items-end">
+                          <Image
+                            className="relative"
+                            src={previewUrl}
+                            alt="Preview"
+                            width={100}
+                            height={50}
+                            layout="responsive"
+                            objectFit="contain"
+                          />
+
+                          <div className="absolute p-3">
+                            <Button
+                              onClick={removeSelectedImage}
+                              className=" bg-white text-black"
+                            >
+                              <Trash />
+                            </Button>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                  <div className="flex justify-end">
+                    <Button disabled={!previewUrl} className="h-10">
+                      Generate
                     </Button>
                   </div>
-                </CardTitle>
-                <CardDescription>
-                  Describe the food, and AI will detect the ingredients.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="grid gap-6">
-                <div className="grid gap-3">
-                  <Input
-                    className="h-31"
-                    id="tabs-demo-name"
-                    placeholder="Орц тодорхойлох"
-                  />
+                </CardContent>
+                <div>
+                  <CardFooter>
+                    <CardDescription>
+                      <div className="flex text-lg font-bold text-black gap-2">
+                        <FileText />
+                        Here is the summary
+                      </div>
+                      Upload a food photo, and AI will detect the ingredients.
+                    </CardDescription>
+                  </CardFooter>
                 </div>
-                <div className="flex justify-end">
-                  <Button className="h-10">Generate</Button>
-                </div>
-              </CardContent>
-              <div>
-                <CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="Ingredient recognition">
+              <Card className="border-none shadow-none ">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex w-full h-fit justify-between">
+                      <div className="flex gap-2 justify-center items-center text-lg font-bold">
+                        <Sparkles />
+                        Ingredient recognition
+                      </div>
+                      <Button
+                        className="w-12 h-10"
+                        variant="outline"
+                        size="icon"
+                        aria-label="Submit"
+                      >
+                        <RotateCw />
+                      </Button>
+                    </div>
+                  </CardTitle>
                   <CardDescription>
-                    <div className="flex text-lg font-bold text-black gap-2">
-                      <FileText />
-                      Identified Ingredients
-                    </div>
-                    First, enter your text to recognize an ingredients.
+                    Describe the food, and AI will detect the ingredients.
                   </CardDescription>
-                </CardFooter>
-              </div>
-            </Card>
-          </TabsContent>
-          <TabsContent value="Image creator">
-            <Card>
-              <CardHeader>
-                <CardTitle>
-                  <div className="flex w-full h-fit justify-between">
-                    <div className="flex gap-2 justify-center items-center text-lg font-bold">
-                      <Sparkles />
-                      Food image creator
-                    </div>
-                    <Button
-                      className="w-12 h-10"
-                      variant="outline"
-                      size="icon"
-                      aria-label="Submit"
-                    >
-                      <RotateCw />
-                    </Button>
+                </CardHeader>
+                <CardContent className="grid gap-6">
+                  <div className="grid gap-3">
+                    <Input
+                      className="h-31"
+                      id="tabs-demo-name"
+                      placeholder="Орц тодорхойлох"
+                    />
                   </div>
-                </CardTitle>
-                <CardDescription>
-                  What food image do you want? Describe it briefly.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="grid gap-6">
-                <div className="grid gap-3">
-                  <Input
-                    className="h-32.5"
-                    id="tabs-demo-name"
-                    placeholder="Хоолны тайлбар"
-                  />
+                  <div className="flex justify-end">
+                    <Button className="h-10">Generate</Button>
+                  </div>
+                </CardContent>
+                <div>
+                  <CardFooter>
+                    <CardDescription>
+                      <div className="flex text-lg font-bold text-black gap-2">
+                        <FileText />
+                        Identified Ingredients
+                      </div>
+                      First, enter your text to recognize an ingredients.
+                    </CardDescription>
+                  </CardFooter>
                 </div>
-                <div className="flex justify-end">
-                  <Button className="h-10">Generate</Button>
-                </div>
-              </CardContent>
-              <div>
-                <CardFooter>
-                  <CardDescription>
-                    <div className="flex text-lg font-bold text-black gap-2">
-                      <FileText />
-                      Result
+              </Card>
+            </TabsContent>
+            <TabsContent value="Image creator">
+              <Card className="border-none shadow-none ">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex w-full h-fit justify-between">
+                      <div className="flex gap-2 justify-center items-center text-lg font-bold">
+                        <Sparkles />
+                        Food image creator
+                      </div>
+                      <Button
+                        className="w-12 h-10"
+                        variant="outline"
+                        size="icon"
+                        aria-label="Submit"
+                      >
+                        <RotateCw />
+                      </Button>
                     </div>
-                    First, enter your text to generate an image.
+                  </CardTitle>
+                  <CardDescription>
+                    What food image do you want? Describe it briefly.
                   </CardDescription>
-                </CardFooter>
-              </div>
-            </Card>
-          </TabsContent>
-        </Tabs>
+                </CardHeader>
+                <CardContent className="grid gap-6">
+                  <div className="grid gap-3">
+                    <Input
+                      className="h-32.5"
+                      id="tabs-demo-name"
+                      placeholder="Хоолны тайлбар"
+                    />
+                  </div>
+                  <div className="flex justify-end">
+                    <Button className="h-10">Generate</Button>
+                  </div>
+                </CardContent>
+                <div>
+                  <CardFooter>
+                    <CardDescription>
+                      <div className="flex text-lg font-bold text-black gap-2">
+                        <FileText />
+                        Result
+                      </div>
+                      First, enter your text to generate an image.
+                    </CardDescription>
+                  </CardFooter>
+                </div>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        </div>
       </div>
     </div>
   );
